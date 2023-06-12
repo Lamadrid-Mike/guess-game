78534232258ec27f921bcf1780cebf8ce2e07384
Mike Lamadrid <mikealamadrid@hotmail.com>
2023-06-11 22:32:34 -0400

    a simple game

---
+++ script.js
@@ -0,0 +1,51 @@
+let displayImage = document.querySelector(".country-flag");
+let countryName = document.querySelector(".country-names");
+
+let fourCountriesIndex = [];
+let randomCountries = [];
+let singleCountry;
+
+const numberGenerator = () => {
+  return Math.floor(Math.random() * 250);
+};
+
+const pushFourCountries = (countriesIndex, array, data) => {
+  countriesIndex.forEach((index) => array.push(data[index]));
+};
+
+const pickFourCountries = () => {
+  for (let i = 0; i < 4; i++) {
+    if (!fourCountriesIndex.includes(numberGenerator())) {
+      fourCountriesIndex.push(numberGenerator());
+    }
+  }
+};
+
+const destructuring = (obj) => {
+  const {
+    flags: { png },
+    name: { common },
+  } = obj[0];
+  displayImage.setAttribute("src", png);
+  singleCountry = common;
+};
+
+const displayOptions = (array, targetHtml) => {
+  array.map((el) => {
+    const html = `<button>${el.name.common}</button>`;
+    targetHtml.insertAdjacentHTML("beforeend", html);
+  });
+};
+
+async function getData() {
+  let countries = await fetch(
+    "https://restcountries.com/v3.1/all?fields=name,flags"
+  );
+  let data = await countries.json();
+  pickFourCountries();
+  pushFourCountries(fourCountriesIndex, randomCountries, data);
+  destructuring(randomCountries);
+  displayOptions(randomCountries, countryName);
+  console.log(randomCountries);
+}
+getData();
